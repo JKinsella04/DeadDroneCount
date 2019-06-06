60ffbed46fcb4f2aa5c0f349bd8475b5f8c293ba
Jack Kinsella <jkinsella@psaschool.org>
2019-06-06 11:54:52 -0400

    html doc will now show  when 0 drones are killed

--- lib/log2.py
+++ lib/log2.py
@@ -31,10 +31,26 @@ def on_press(key):
             f.close
             f.newline
 
-        if key == keyboard.Key.esc:
+        if key == keyboard.Key.f1:
             drone = 0
             os.system("cls")
             print(drone)
+            f = open("dronecount.html", "w")
+            f.write('<body style="background-color:powderblue;">')
+            f.write('<h1 style="color:black;font-size:40px;">')
+            f.write('<center>')
+            f.write('Killed Drone Counter\n')
+            f.write('</center>')
+            f.write('</h1>')
+            f.write('<center>')
+            f.write('<h2 style="color:black;font-size:200px;">')
+            f.write(str(drone))
+            f.write('</center>')
+            f.write('</h2>\n')
+            f.write('<meta http-equiv="refresh" content="1">')
+            f.write('</body>')
+            f.close
+            f.newline
     except AttributeError:
         print(drone)
 
